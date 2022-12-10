dcaffb509daa937c830ef5d712d3a13e65b59c45
unlu <corepavlion@gmail.com>
2022-12-11 00:25:36 +0300

    My Coroutine Adventure - not cancellable & global scope

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,22 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        val deferred: List<Deferred<List<User>>> = repos.map { repo ->
+            GlobalScope.async { // global scopeda basladigi icin cancel a bassakta istekler gider
+                log("starting loading for ${repo.name}")
+                delay(3000)
+                service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+            }
+
+        }
+       return deferred.awaitAll().flatten().aggregate()
 }
